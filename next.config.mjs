fd680c952a9c699b0d3780708f255e5c9d77219d
Óscar Giménez Aldabas <oscar.gimenez@bcncgroup.com>
2024-03-19 10:37:02 +0100

    feat: :sparkles: finish microfrontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name: "SushiMicroFrontendReviews",
+        filename: "static/chunks/remoteEntry.js",
+
+        exposes: {
+          "./SushiMicroFrontendReviews": "./src/app/[id]/page.tsx",
+        },
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+      Include an ID please
+    </main>
+  );
+}
